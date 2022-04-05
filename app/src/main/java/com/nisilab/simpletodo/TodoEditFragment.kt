8768ebdca03352a76a3d393ab67a40902d3ab5ef
nissy-yuki <nisi20010510@gmail.com>
2022-04-05 14:47:17 +0900

    adapt databinding & write fragment.xml

--- app/src/main/java/com/nisilab/simpletodo/TodoEditFragment.kt
+++ app/src/main/java/com/nisilab/simpletodo/TodoEditFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.nisilab.simpletodo.databinding.FragmentTodoListBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +35,11 @@ class TodoEditFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_todo_edit, container, false)
+        val binding = DataBindingUtil.inflate<FragmentTodoListBinding>(inflater,
+            R.layout.fragment_todo_list,container,false)
+
+
+        return binding.root
     }
 
     companion object {
