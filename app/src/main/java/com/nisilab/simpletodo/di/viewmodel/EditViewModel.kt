eeea494421f6a46e76c580d14cee69d561df77b1
nissy-yuki <nisi20010510@gmail.com>
2022-04-06 22:32:58 +0900

    write viewmodel

--- app/src/main/java/com/nisilab/simpletodo/di/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/nisilab/simpletodo/di/viewmodel/EditViewModel.kt
@@ -1,10 +1,41 @@
 package com.nisilab.simpletodo.di.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nisilab.simpletodo.di.database.TodoItem
 import com.nisilab.simpletodo.di.repository.DataRepository
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 import javax.inject.Inject
 
 class EditViewModel@Inject constructor(
-    repository: DataRepository
+    private val repository: DataRepository
 ): ViewModel() {
+
+    private val _editTitle: MutableLiveData<String> = MutableLiveData()
+    private val _editDeadLine: MutableLiveData<LocalDateTime> = MutableLiveData()
+    private val _editTag: MutableLiveData<String> = MutableLiveData()
+    private val _editText: MutableLiveData<String> = MutableLiveData()
+
+    val editTitle: LiveData<String> = _editTitle
+    val editDeadLine: LiveData<LocalDateTime> = _editDeadLine
+    val editTag: LiveData<String> = _editTag
+    val editText: LiveData<String> = _editText
+
+    fun addItem(){
+        viewModelScope.launch {
+            repository.addItem(
+                TodoItem(
+                    id = 0,
+                    title = editTitle.value!!,
+                    deadLine = editDeadLine.value!!,
+                    tag = editTag.value,
+                    text = editText.value
+                )
+            )
+        }
+    }
+
 }
--- app/src/main/java/com/nisilab/simpletodo/di/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/nisilab/simpletodo/di/viewmodel/ListViewModel.kt
@@ -1,11 +1,36 @@
 package com.nisilab.simpletodo.di.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nisilab.simpletodo.di.database.TodoItem
 import com.nisilab.simpletodo.di.repository.DataRepository
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ListViewModel @Inject constructor(
-    repository: DataRepository
+    private val repository: DataRepository
 ): ViewModel() {
+    private val _todoItems: MutableLiveData<List<TodoItem>> = MutableLiveData()
 
+    val todoItems: LiveData<List<TodoItem>> = _todoItems
+
+    fun getAllItems(){
+        viewModelScope.launch {
+            _todoItems.value = repository.getItems()
+        }
+    }
+
+    fun updateItem(item: TodoItem){
+        viewModelScope.launch {
+            repository.updateItem(item)
+        }
+    }
+
+    fun deleteItem(item: TodoItem){
+        viewModelScope.launch {
+            repository.deleteItem(item)
+        }
+    }
 }
