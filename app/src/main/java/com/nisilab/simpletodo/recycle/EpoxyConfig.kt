b18449f91fe06442f52a6b8d1949e5936a0ccde3
nissy-yuki <nisi20010510@gmail.com>
2022-04-10 22:05:25 +0900

    add no data text

--- app/src/main/java/com/nisilab/simpletodo/recycle/EpoxyConfig.kt
+++ app/src/main/java/com/nisilab/simpletodo/recycle/EpoxyConfig.kt
@@ -3,6 +3,6 @@ package com.nisilab.simpletodo.recycle
 import com.airbnb.epoxy.EpoxyDataBindingLayouts
 import com.nisilab.simpletodo.R
 
-@EpoxyDataBindingLayouts(R.layout.todo_item)
+@EpoxyDataBindingLayouts(R.layout.todo_item,R.layout.no_data_item)
 
 interface EpoxyConfig {}
--- app/src/main/java/com/nisilab/simpletodo/recycle/ItemDataBindingViewController.kt
+++ app/src/main/java/com/nisilab/simpletodo/recycle/ItemDataBindingViewController.kt
@@ -3,22 +3,30 @@ package com.nisilab.simpletodo.recycle
 import android.view.View
 import com.airbnb.epoxy.Typed2EpoxyController
 import com.nisilab.simpletodo.di.database.TodoItem
+import com.nisilab.simpletodo.noDataItem
 import com.nisilab.simpletodo.todoItem
 
 class ItemDataBindingViewController(
     private val selectListener: SelectListener
 ): Typed2EpoxyController<List<RecycleItem>,Boolean>() {
     override fun buildModels(items: List<RecycleItem>, loadingMore: Boolean) {
-        items.forEach { item ->
-            todoItem {
-                title(item.title)
-                deadLine(item.deadLine.toString())
-                tag(item.tag)
-                text(item.text)
-                onClickOpenButton( View.OnClickListener { selectListener.onClickOpenButton(item) } )
-                onClickCloseButton( View.OnClickListener { selectListener.onClickCloseButton(item) } )
+        if(!items.isNullOrEmpty()){
+            items.forEach { item ->
+                todoItem {
+                    title(item.title)
+                    deadLine(item.deadLine.toString())
+                    tag(item.tag)
+                    text(item.text)
+                    onClickOpenButton( View.OnClickListener { selectListener.onClickOpenButton(item) } )
+                    onClickCloseButton( View.OnClickListener { selectListener.onClickCloseButton(item) } )
+                }
+            }
+        }else{
+            noDataItem {
+                text("no data")
             }
         }
+
     }
 
     interface SelectListener {
